41c3628560d6f86e39c70ac6d7fbc5265b3644d2
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2024-08-14 19:07:21 -0400

    New: Added ConfigHandler

---
+++ src/main/java/net/pldyn/gptradeproperty/ConfigHandler.java
@@ -0,0 +1,72 @@
+package net.pldyn.gptradeproperty;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+/**
+ * Handles the configuration of the plugin
+ */
+public class ConfigHandler {
+  private static final Logger Log = Logger.getLogger("GP-TradeProperty");
+
+  private static File coreConfigFile;
+  private static FileConfiguration coreConfig;
+
+  /**
+   * Initialize the configuration
+   */
+  public static void confInit() {
+    // Prep files
+    fileInit();
+    coreConfig.options().copyDefaults(true);
+    saveConfigs();
+
+    createDefaults();
+  }
+
+  /**
+   * Initialize the files
+   */
+  private static void fileInit() {
+    coreConfigFile = new File(
+        Bukkit.getPluginManager()
+            .getPlugin( "GP-TradeProperty" )
+            .getDataFolder(), "config.yml"
+    );
+
+    if ( ! coreConfigFile.exists() ) {
+      try {
+        coreConfigFile.createNewFile();
+      } catch ( IOException e ) {
+        Log.warning( "Failed to create config file!" );
+      }
+    }
+
+    coreConfig = YamlConfiguration.loadConfiguration( coreConfigFile );
+  }
+
+  /**
+   * Create defaults
+   */
+  private static void createDefaults() {
+    coreConfig.addDefault( "enabledWorlds", new ArrayList<String>() );
+    saveConfigs();
+  }
+
+  /**
+   * Save the configuration files
+   */
+  private static void saveConfigs() {
+    try {
+      coreConfig.save( coreConfigFile );
+    } catch ( IOException e ) {
+      Log.warning( "Failed to save config file!" );
+    }
+  }
+}
--- src/main/java/net/pldyn/gptradeproperty/gpTradeProperty.java
+++ src/main/java/net/pldyn/gptradeproperty/gpTradeProperty.java
@@ -1,17 +1,49 @@
 package net.pldyn.gptradeproperty;
 
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
+/**
+ * Main class of the plugin
+ */
 public final class gpTradeProperty extends JavaPlugin {
+
+  private static final Logger Log = Logger.getLogger("GP-TradeProperty");
+  private GriefPrevention GP;
 
   @Override
   public void onEnable() {
     // Plugin startup logic
+    Log.info("GP Trade Property Plugin Enabled");
 
+    // Get GP online
+    if (getServer().getPluginManager().getPlugin("GriefPrevention") == null) {
+      Log.warning("GriefPrevention not found, disabling plugin");
+      getServer().getPluginManager().disablePlugin(this);
+    }
+
+    ConfigHandler.confInit();
   }
 
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+    Log.info("GP Trade Property Plugin Disabled");
+  }
+
+  /**
+   * Get instance of the plugin
+   */
+  public gpTradeProperty getInstance() {
+    return this;
+  }
+
+  /**
+   * Initialize the plugin
+   */
+  private void doInit(GriefPrevention gp) {
+    Log.info( "GP Trade Property Plugin Initialized" );
   }
 }
